3d5c7057fbf78293f606e4e5deb61bece1ea8129
Daniel Rose <daniel-rose@gmx.de>
2020-10-11 20:28:06 +0200

    add more code coverage

--- tests/FondOfSpryker/Zed/CollaborativeCart/Business/Model/CartClaimerTest.php
+++ tests/FondOfSpryker/Zed/CollaborativeCart/Business/Model/CartClaimerTest.php
@@ -122,8 +122,305 @@ class CartClaimerTest extends Unit
         $this->permissionFacadeMock->expects(self::atLeastOnce())
             ->method('can')
             ->with(CollaborateCartPermissionPlugin::KEY, $idCompanyUser)
-            ->willReturn($idCompanyUser);
+            ->willReturn(true);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('getCompanyUserReference')
+            ->willReturn($currentCompanyUserReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setOriginalCompanyUserReference')
+            ->with($currentCompanyUserReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('getCustomerReference')
+            ->willReturn($currentCustomerReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setOriginalCustomerReference')
+            ->with($currentCustomerReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->companyUserTransferMock->expects(self::atLeastOnce())
+            ->method('getCompanyUserReference')
+            ->willReturn($companyUserReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setCompanyUserReference')
+            ->with($companyUserReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->claimCartRequestTransferMock->expects(self::atLeastOnce())
+            ->method('getNewCustomerReference')
+            ->willReturn($customerReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setCustomerReference')
+            ->with($customerReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->quoteWriterMock->expects(self::atLeastOnce())
+            ->method('update')
+            ->with($this->quoteTransferMock)
+            ->willReturn($this->quoteTransferMock);
 
         $claimCartResponseTransfer = $this->cartClaimer->claim($this->claimCartRequestTransferMock);
+
+        self::assertTrue($claimCartResponseTransfer->getIsSuccess());
+        self::assertEquals(null, $claimCartResponseTransfer->getError());
+        self::assertEquals($this->quoteTransferMock, $claimCartResponseTransfer->getQuote());
+    }
+
+    /**
+     * @return void
+     */
+    public function testClaimWithUpdateError(): void
+    {
+        $idCompanyUser = 1;
+        $companyUserReference = 'DE-CU-1';
+        $currentCompanyUserReference = 'DE-CU-2';
+        $customerReference = 'DE-CU-1';
+        $currentCustomerReference = 'DE-CU-2';
+
+        $this->quoteReaderMock->expects(self::atLeastOnce())
+            ->method('getByClaimCartRequest')
+            ->with($this->claimCartRequestTransferMock)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->companyUserReaderMock->expects(self::atLeastOnce())
+            ->method('getActiveByClaimCartRequestAndQuote')
+            ->with($this->claimCartRequestTransferMock, $this->quoteTransferMock)
+            ->willReturn($this->companyUserTransferMock);
+
+        $this->companyUserTransferMock->expects(self::atLeastOnce())
+            ->method('getIdCompanyUser')
+            ->willReturn($idCompanyUser);
+
+        $this->permissionFacadeMock->expects(self::atLeastOnce())
+            ->method('can')
+            ->with(CollaborateCartPermissionPlugin::KEY, $idCompanyUser)
+            ->willReturn(true);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('getCompanyUserReference')
+            ->willReturn($currentCompanyUserReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setOriginalCompanyUserReference')
+            ->with($currentCompanyUserReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('getCustomerReference')
+            ->willReturn($currentCustomerReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setOriginalCustomerReference')
+            ->with($currentCustomerReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->companyUserTransferMock->expects(self::atLeastOnce())
+            ->method('getCompanyUserReference')
+            ->willReturn($companyUserReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setCompanyUserReference')
+            ->with($companyUserReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->claimCartRequestTransferMock->expects(self::atLeastOnce())
+            ->method('getNewCustomerReference')
+            ->willReturn($customerReference);
+
+        $this->quoteTransferMock->expects(self::atLeastOnce())
+            ->method('setCustomerReference')
+            ->with($customerReference)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->quoteWriterMock->expects(self::atLeastOnce())
+            ->method('update')
+            ->with($this->quoteTransferMock)
+            ->willReturn(null);
+
+        $claimCartResponseTransfer = $this->cartClaimer->claim($this->claimCartRequestTransferMock);
+
+        self::assertFalse($claimCartResponseTransfer->getIsSuccess());
+        self::assertNotEquals(null, $claimCartResponseTransfer->getError());
+        self::assertEquals(null, $claimCartResponseTransfer->getQuote());
+    }
+
+    /**
+     * @return void
+     */
+    public function testClaimWithWrongPermission(): void
+    {
+        $idCompanyUser = 1;
+
+        $this->quoteReaderMock->expects(self::atLeastOnce())
+            ->method('getByClaimCartRequest')
+            ->with($this->claimCartRequestTransferMock)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->companyUserReaderMock->expects(self::atLeastOnce())
+            ->method('getActiveByClaimCartRequestAndQuote')
+            ->with($this->claimCartRequestTransferMock, $this->quoteTransferMock)
+            ->willReturn($this->companyUserTransferMock);
+
+        $this->companyUserTransferMock->expects(self::atLeastOnce())
+            ->method('getIdCompanyUser')
+            ->willReturn($idCompanyUser);
+
+        $this->permissionFacadeMock->expects(self::atLeastOnce())
+            ->method('can')
+            ->with(CollaborateCartPermissionPlugin::KEY, $idCompanyUser)
+            ->willReturn(false);
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('getCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setOriginalCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('getCustomerReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setOriginalCustomerReference');
+
+        $this->companyUserTransferMock->expects(self::never())
+            ->method('getCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setCompanyUserReference');
+
+        $this->claimCartRequestTransferMock->expects(self::never())
+            ->method('getNewCustomerReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setCustomerReference');
+
+        $this->quoteWriterMock->expects(self::never())
+            ->method('update');
+
+        $claimCartResponseTransfer = $this->cartClaimer->claim($this->claimCartRequestTransferMock);
+
+        self::assertFalse($claimCartResponseTransfer->getIsSuccess());
+        self::assertNotEquals(null, $claimCartResponseTransfer->getError());
+        self::assertEquals(null, $claimCartResponseTransfer->getQuote());
+    }
+
+    /**
+     * @return void
+     */
+    public function testClaimWithNonExistingQuote(): void
+    {
+        $this->quoteReaderMock->expects(self::atLeastOnce())
+            ->method('getByClaimCartRequest')
+            ->with($this->claimCartRequestTransferMock)
+            ->willReturn(null);
+
+        $this->companyUserReaderMock->expects(self::never())
+            ->method('getActiveByClaimCartRequestAndQuote');
+
+        $this->companyUserTransferMock->expects(self::never())
+            ->method('getIdCompanyUser');
+
+        $this->permissionFacadeMock->expects(self::never())
+            ->method('can');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('getCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setOriginalCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('getCustomerReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setOriginalCustomerReference');
+
+        $this->companyUserTransferMock->expects(self::never())
+            ->method('getCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setCompanyUserReference');
+
+        $this->claimCartRequestTransferMock->expects(self::never())
+            ->method('getNewCustomerReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setCustomerReference');
+
+        $this->quoteWriterMock->expects(self::never())
+            ->method('update');
+
+        $claimCartResponseTransfer = $this->cartClaimer->claim($this->claimCartRequestTransferMock);
+
+        self::assertFalse($claimCartResponseTransfer->getIsSuccess());
+        self::assertNotEquals(null, $claimCartResponseTransfer->getError());
+        self::assertEquals(null, $claimCartResponseTransfer->getQuote());
+    }
+
+    /**
+     * @return void
+     */
+    public function testClaimWithNonExistingCompanyUser(): void
+    {
+        $idCompanyUser = 1;
+        $companyUserReference = 'DE-CU-1';
+        $currentCompanyUserReference = 'DE-CU-2';
+        $customerReference = 'DE-CU-1';
+        $currentCustomerReference = 'DE-CU-2';
+
+        $this->quoteReaderMock->expects(self::atLeastOnce())
+            ->method('getByClaimCartRequest')
+            ->with($this->claimCartRequestTransferMock)
+            ->willReturn($this->quoteTransferMock);
+
+        $this->companyUserReaderMock->expects(self::atLeastOnce())
+            ->method('getActiveByClaimCartRequestAndQuote')
+            ->with($this->claimCartRequestTransferMock, $this->quoteTransferMock)
+            ->willReturn(null);
+
+        $this->companyUserTransferMock->expects(self::never())
+            ->method('getIdCompanyUser');
+
+        $this->permissionFacadeMock->expects(self::never())
+            ->method('can');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('getCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setOriginalCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('getCustomerReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setOriginalCustomerReference');
+
+        $this->companyUserTransferMock->expects(self::never())
+            ->method('getCompanyUserReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setCompanyUserReference');
+
+        $this->claimCartRequestTransferMock->expects(self::never())
+            ->method('getNewCustomerReference');
+
+        $this->quoteTransferMock->expects(self::never())
+            ->method('setCustomerReference');
+
+        $this->quoteWriterMock->expects(self::never())
+            ->method('update');
+
+        $claimCartResponseTransfer = $this->cartClaimer->claim($this->claimCartRequestTransferMock);
+
+        self::assertFalse($claimCartResponseTransfer->getIsSuccess());
+        self::assertNotEquals(null, $claimCartResponseTransfer->getError());
+        self::assertEquals(null, $claimCartResponseTransfer->getQuote());
     }
 }
